964b1e2ed96eeabefb1a9ec5ea01f29df9d71432
tong-huat <tonghuat86@gmail.com>
2021-08-29 23:37:06 +0800

    setup auth CB

--- index.js
+++ index.js
@@ -35,16 +35,78 @@ if (process.env.ENV === 'PRODUCTION') {
     user: 'midzham',
     host: 'localhost',
     database: 'makanwhere',
-    port: 5432, // Postgres server always runs on this port by default
+    port: 5432,
   };
 }
 const pool = new Pool(pgConnectionConfigs);
 
+// Auth Middleware
+const getHash = (input) => {
+  // create new SHA object
+  const shaObj = new jsSHA('SHA-512', 'TEXT', { encoding: 'UTF8' });
+
+  // create an unhashed cookie string based on user ID and salt
+  const unhashedString = `${input}-${SALT}`;
+
+  // generate a hashed cookie string using SHA object
+  shaObj.update(unhashedString);
+
+  return shaObj.getHash('HEX');
+};
+
+app.use((request, response, next) => {
+  // set the default value
+  request.isUserLoggedIn = false;
+
+  // check to see if the cookies you need exists
+  if (request.cookies.loggedIn && request.cookies.userId) {
+    // get the hased value that should be inside the cookie
+    const hash = getHash(request.cookies.userId);
+
+    // test the value of the cookie
+    if (request.cookies.loggedIn === hash) {
+      request.isUserLoggedIn = true;
+    }
+  }
+
+  next();
+});
+
+// Auth middleware to restrict users only access
+const restrictToLoggedIn = (request, response, next) => {
+  // is the user logged in? Use the other middleware.
+  if (request.loggedIn === false) {
+    response.redirect('/login');
+  } else {
+    // The user is logged in. Get the user from the DB.
+    const userQuery = 'SELECT * FROM users WHERE id=$1';
+    pool.query(userQuery, [request.cookies.userId])
+      .then((userQueryResult) => {
+        // can't find the user based on their cookie.
+        if (userQueryResult.rows.length === 0) {
+          response.redirect('/login');
+          return;
+        }
+
+        // attach the DB query result to the request object.
+        request.user = userQueryResult.rows[0];
+
+        // go to the route callback.
+        next();
+      }).catch((error) => {
+        response.redirect('/login');
+      });
+  }
+};
+
+// CB for routes
+// CB to render welcome page
 const renderWelcomePage = (request, response) => {
   console.log('welcome request came in');
   response.render('welcome');
 };
 
+// CB to render all listing page
 const renderEstIndex = (request, response) => {
   console.log('index request came in');
 
@@ -67,16 +129,31 @@ const renderEstIndex = (request, response) => {
   pool.query('SELECT * from establishments', listAllEstablishment);
 };
 
+// CB to render registration page
 const renderRegistration = (request, response) => {
+  if (request.isUserLoggedIn === true) {
+    response.redirect('/listing');
+    return;
+  }
   console.log('registration request came in');
   response.render('register');
 };
 
 // CB to retrieve user's data for registration
 const registerUser = (request, response) => {
+  if (request.isUserLoggedIn === true) {
+    response.redirect('/listing');
+    return;
+  }
   console.log('retrieving user data');
   // initialise the jsSHA object
   const shaObj = new jsSHA('SHA-512', 'TEXT', { encoding: 'UTF8' });
+
+  // TODO: further form validation
+  if (!request.body.email || !request.body.password) {
+    response.redirect('/register');
+  }
+
   // input the password from the request to the SHA object
   shaObj.update(request.body.password);
   // get the hashed password as output from the SHA object
@@ -96,13 +173,22 @@ const registerUser = (request, response) => {
   });
 };
 
+// CB to render login page
 const renderLogin = (request, response) => {
+  if (request.isUserLoggedIn === true) {
+    response.redirect('/listing');
+    return;
+  }
   console.log('login request came in');
   response.render('login');
 };
 
 // CB to verify login details and login to acct if successful verification
 const loginAccount = (request, response) => {
+  if (request.isUserLoggedIn === true) {
+    response.redirect('/listing');
+    return;
+  }
   console.log('trying to login');
   const values = [request.body.username];
   console.log(values);
@@ -115,6 +201,7 @@ const loginAccount = (request, response) => {
 
     //  we didnt find a user with that email.
     if (result.rows.length === 0) {
+      // eslint-disable-next-line max-len
       /* the error for password and user are the same. don't tell the user which error they got for security reasons, otherwise people can guess if a person is a user of a given service. */
       response.status(403).send('sorry!');
       return;
@@ -135,18 +222,28 @@ const loginAccount = (request, response) => {
       const hashedCookieString = shaObj1.getHash('HEX');
       response.cookie('loggedInHash', hashedCookieString);
       response.cookie('userId', result.rows[0].id);
-      response.redirect('/listings');
+      response.redirect('/listing');
     } else {
       response.status(403).send('not successful');
     }
   });
 };
 
+// CB to del cookies and logout
+const logout = (request, response) => {
+  // Remove cookies from response header to log out
+  response.clearCookie('loggedIn');
+  response.clearCookie('userId');
+  response.clearCookie('loggedInHash');
+  response.redirect('/');
+};
+
 app.get('/', renderWelcomePage);
-app.get('/listings', renderEstIndex);
+app.get('/listing', restrictToLoggedIn, renderEstIndex);
 app.get('/register', renderRegistration);
 app.post('/register', registerUser);
 app.get('/login', renderLogin);
 app.post('/login', loginAccount);
-
+// logs the user out
+app.get('/logout', logout);
 app.listen(3004);
