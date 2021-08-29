ea903cf63e9f785d4dbab245f60093e9fd01ebaf
tong-huat <tonghuat86@gmail.com>
2021-08-29 13:50:32 +0800

    set up basics

---
+++ index.js
@@ -0,0 +1,44 @@
+import express, { request, response } from 'express';
+import methodOverride from 'method-override';
+// eslint-disable-next-line import/no-unresolved
+import pg from 'pg';
+import jsSHA from 'jssha';
+import cookieParser from 'cookie-parser';
+
+// const PORT = process.argv[2];
+
+const app = express();
+const SALT = 'i like cocomelon';
+
+app.set('view engine', 'ejs');
+// Override POST requests with query param ?_method=PUT to be PUT requests
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.use(cookieParser());
+
+// Initialise DB connection
+const { Pool } = pg;
+let pgConnectionConfigs;
+if (process.env.ENV === 'PRODUCTION') {
+  // determine how we connect to the remote Postgres server
+  pgConnectionConfigs = {
+    user: 'postgres',
+    // set DB_PASSWORD as an environment variable for security.
+    password: process.env.DB_PASSWORD,
+    host: 'localhost',
+    database: 'birding',
+    port: 5432,
+  };
+} else {
+  pgConnectionConfigs = {
+    user: 'midzham',
+    host: 'localhost',
+    database: 'birding',
+    port: 5432, // Postgres server always runs on this port by default
+  };
+}
+const pool = new Pool(pgConnectionConfigs);
+
+
+app.listen(3004);
